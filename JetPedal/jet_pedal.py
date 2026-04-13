5c48114c89528bd8d4c933b672eda0a2499d5b5e
John Drake <foxox@users.noreply.github.com>
2026-04-12 21:00:56 -0400

    Updated jet pedal to retry joystick setup if joystick not detected

--- JetPedal/jet_pedal.py
+++ JetPedal/jet_pedal.py
@@ -11,7 +11,9 @@ pygame.joystick.init()
 # Config:
 
 # scroll lock seems to cause problems, home seems to be numpad 7
-jet_key = 'm'
+jet_key = 'scrlk'
+# canonical names: https://github.com/boppreh/keyboard/blob/master/keyboard/_canonical_names.py#L11
+# avoid letter keys which interfere with typing in the chat box
 
 pwm_wavelength = 0.05
 
@@ -30,98 +32,104 @@ def mouseHook(event):
     mouse_override_from_events = False if event.event_type == mouse.UP else True
 mouse.hook(mouseHook)
 
-if pygame.joystick.get_count() > 0:
-  joystick = pygame.joystick.Joystick(0)
-  joystick.init()
-
-  pedal_value = 1.0 # 1.0 is "off" to start
-  tps_timer_start = time.time()
-  pwm_cycle_start = tps_timer_start
-  ticks = 0
-  # print('reset')
-  latest_tps = 0
-  jet_output = 0
-
-  last_jet_output = 0
-
-  running = True
-  while running:
-    
-    # Reduce CPU utilization
-    time.sleep(1.0/1000.0)
-
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
-      # elif event.type == pygame.JOYBUTTONDOWN:
-      #   print(f"Button {event.button} pressed")
-      # elif event.type == pygame.JOYBUTTONUP:
-      #   print(f"Button {event.button} released")
-      elif event.type == pygame.JOYAXISMOTION:
-        axis = event.axis
-        # print(f"Axis {axis} value: {event.value}")
-        if axis == jet_pedal_axis:
-          # print(f"Axis {axis} value: {event.value}")
-          pedal_value = event.value
-    
-    
-    # convert joystick 1 to -1 to range 0 to 1
-    jet_control_input = -0.5 * (pedal_value - 1.0)
-    jet_control_input = np.clip(0, 1, jet_control_input)
-    if jet_control_input < 0.07:
-      jet_control_input = 0.0
-    if jet_control_input > 1.0 - 0.05:
-      jet_control_input = 1.0
-    # print(pedal_value)
-    # print(jet_control_input)
-
-    new_time = time.time()
-    tps_time_diff = new_time - tps_timer_start
-    if (tps_time_diff > 1.0):
-      latest_tps = ticks / tps_time_diff
-      # print('latest_tps:', latest_tps)
-      tps_timer_start = new_time
-      ticks = 0
-    
-    if latest_tps > 10:
-      pwm_delta = new_time - pwm_cycle_start
-      pwm_factor = pwm_delta / pwm_wavelength
-      pwm_factor_adjusted = np.clip(0.0,1.0,pwm_factor)
-      # if pwm_factor_adjusted < 0.07:
-      #   pwm_factor_adjusted = 0.0
-      # if pwm_factor_adjusted > 1.0 - 0.05:
-      #   pwm_factor_adjusted = 1.0
-      jet_output = 0 if jet_control_input <= 0.05 or pwm_factor_adjusted > jet_control_input else 1
-      # if jet_output == 0 and last_jet_output == 1:
-        # print('jet output 0 and last jet output 1','pwm_factor_adjusted:',pwm_factor_adjusted,'jet_control_input:',jet_control_input)
-      if pwm_factor >= 1.0:
-        pwm_factor -= math.floor(pwm_factor)
-        pwm_cycle_start = new_time + pwm_factor * pwm_wavelength # no lost time
-      # print('jet_control_input:',jet_control_input,'pwm_delta:',pwm_delta,'jet_output:',jet_output)
-    else:
-      # print('latest_tps',latest_tps,'<= 10')
-      jet_output = 0
-
-    ticks += 1
-
-    # Handle mouse override
-    if mouse_override_from_events or mouse.is_pressed('right'):
-      # print('Mouse override.')
-      jet_output = 1.0
-
-    # control jets for T2 (mouse or keyboard)
-    if jet_output == 0 and last_jet_output == 1: # falling edge
-      # print('release')
-      # mouse.release('right')
-      keyboard.release(jet_key)
-    elif jet_output == 1 and last_jet_output == 0: # rising edge
-      # print('press')
-      # mouse.press('right')
-      keyboard.press(jet_key)
-    last_jet_output = jet_output
-    
-
-  pygame.joystick.quit()
+while pygame.joystick.get_count() < 1:
+  print("Waiting for joystick/gamepad...")
   pygame.quit()
-else:
-  print("No gamepad found.")
+  pygame.init()
+  pygame.joystick.init()
+  time.sleep(1)
+
+print("Joystick/gamepad detected. Running...")
+
+joystick = pygame.joystick.Joystick(0)
+joystick.init()
+
+pedal_value = 1.0 # 1.0 is "off" to start
+tps_timer_start = time.time()
+pwm_cycle_start = tps_timer_start
+ticks = 0
+# print('reset')
+latest_tps = 0
+jet_output = 0
+
+last_jet_output = 0
+
+running = True
+while running:
+  
+  # Reduce CPU utilization
+  time.sleep(1.0/1000.0)
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+    # elif event.type == pygame.JOYBUTTONDOWN:
+    #   print(f"Button {event.button} pressed")
+    # elif event.type == pygame.JOYBUTTONUP:
+    #   print(f"Button {event.button} released")
+    elif event.type == pygame.JOYAXISMOTION:
+      axis = event.axis
+      # print(f"Axis {axis} value: {event.value}")
+      if axis == jet_pedal_axis:
+        # print(f"Axis {axis} value: {event.value}")
+        pedal_value = event.value
+  
+  
+  # convert joystick 1 to -1 to range 0 to 1
+  jet_control_input = -0.5 * (pedal_value - 1.0)
+  jet_control_input = np.clip(0, 1, jet_control_input)
+  if jet_control_input < 0.07:
+    jet_control_input = 0.0
+  if jet_control_input > 1.0 - 0.05:
+    jet_control_input = 1.0
+  # print(pedal_value)
+  # print(jet_control_input)
+
+  new_time = time.time()
+  tps_time_diff = new_time - tps_timer_start
+  if (tps_time_diff > 1.0):
+    latest_tps = ticks / tps_time_diff
+    # print('latest_tps:', latest_tps)
+    tps_timer_start = new_time
+    ticks = 0
+  
+  if latest_tps > 10:
+    pwm_delta = new_time - pwm_cycle_start
+    pwm_factor = pwm_delta / pwm_wavelength
+    pwm_factor_adjusted = np.clip(0.0,1.0,pwm_factor)
+    # if pwm_factor_adjusted < 0.07:
+    #   pwm_factor_adjusted = 0.0
+    # if pwm_factor_adjusted > 1.0 - 0.05:
+    #   pwm_factor_adjusted = 1.0
+    jet_output = 0 if jet_control_input <= 0.05 or pwm_factor_adjusted > jet_control_input else 1
+    # if jet_output == 0 and last_jet_output == 1:
+      # print('jet output 0 and last jet output 1','pwm_factor_adjusted:',pwm_factor_adjusted,'jet_control_input:',jet_control_input)
+    if pwm_factor >= 1.0:
+      pwm_factor -= math.floor(pwm_factor)
+      pwm_cycle_start = new_time + pwm_factor * pwm_wavelength # no lost time
+    # print('jet_control_input:',jet_control_input,'pwm_delta:',pwm_delta,'jet_output:',jet_output)
+  else:
+    # print('latest_tps',latest_tps,'<= 10')
+    jet_output = 0
+
+  ticks += 1
+
+  # Handle mouse override
+  if mouse_override_from_events or mouse.is_pressed('right'):
+    # print('Mouse override.')
+    jet_output = 1.0
+
+  # control jets for T2 (mouse or keyboard)
+  if jet_output == 0 and last_jet_output == 1: # falling edge
+    # print('release')
+    # mouse.release('right')
+    keyboard.release(jet_key)
+  elif jet_output == 1 and last_jet_output == 0: # rising edge
+    # print('press')
+    # mouse.press('right')
+    keyboard.press(jet_key)
+  last_jet_output = jet_output
+  
+
+pygame.joystick.quit()
+pygame.quit()
